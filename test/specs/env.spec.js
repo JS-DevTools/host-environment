d50a40a4673e9213be36d2bc99603771c689dac0
James Messinger <james.messinger@gmail.com>
2019-02-08 06:15:06 -0600

    Added tests for `host.env`

---
+++ test/specs/env.spec.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const host = require("../../");
+const { expect } = require("chai");
+
+describe("Environment variables", function () {
+
+  it("host.env should be an object", function () {
+    expect(host.env).to.be.an("object");
+  });
+
+  it("all keys should be valid environment variable names", function () {
+    let allowedCharacters;
+
+    if (host.os.windows) {
+      allowedCharacters = /^[a-zA-Z0-9_()-]+$/;
+    }
+    else {
+      allowedCharacters = /^[a-zA-Z0-9_%-]+$/;
+    }
+
+    for (let key of Object.keys(host.env)) {
+      expect(key).to.match(allowedCharacters);
+    }
+  });
+
+  it("all values should be strings", function () {
+    for (let key of Object.keys(host.env)) {
+      let value = host.env[key];
+      expect(value).to.be.a("string");
+    }
+  });
+
+});

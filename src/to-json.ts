7429931966e5ae50c4c1058a9aeb8363e9cc0b15
James Messinger <james.messinger@gmail.com>
2019-02-08 07:21:35 -0600

    Improved the `toJSON()` method so it only returns the relevant properties

--- src/to-json.ts
+++ src/to-json.ts
@@ -4,11 +4,15 @@ import { Host, HostJSON } from "./host";
  * Returns a copy of the host object that can be safely converted to JSON.
  */
 export function toJSON(this: Host): HostJSON {
-  let clone: HostJSON = Object.assign({}, this, {
+  let clone: HostJSON = {
     // The global object can't be serialized to JSON because it has circular references.
     // So call just serialize its toString() representation instead
-    global: Object.prototype.toString.call(this.global)
-  });
+    global: Object.prototype.toString.call(this.global),
+    os: this.os,
+    env: this.env,
+    node: this.node,
+    browser: this.browser,
+  };
 
   return clone;
 }

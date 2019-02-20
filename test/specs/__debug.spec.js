036b5e4ed76575ebe5804d1e2da9d317603a5331
James Messinger <james.messinger@gmail.com>
2019-02-20 06:13:57 -0600

    Added debug logging

---
+++ test/specs/__debug.spec.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const { host } = require("../../");
+
+console.log("\n==================== BEGIN DEBUG INFO ====================");
+
+if (typeof window === "object") {
+  console.log(`\nUSER AGENT:\n ${navigator.userAgent}`);
+}
+
+let pojo = host.toJSON();
+pojo.env = Object.keys(pojo.env); // Don't log the values, which may have sensitive info
+console.log(`\nHOST:\n ${JSON.stringify(pojo, undefined, 2)}`);
+
+console.log("\n==================== END DEBUG INFO ====================");

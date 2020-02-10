ff8bca650bfad063ffb385bc5301cd3baad72692
James Messinger <james.messinger@gmail.com>
2020-02-09 18:18:45 -0600

    Upgraded Karma-Config and updated the GitHub Actions config to use SauceLabs for Safari and Edge

--- karma.conf.js
+++ karma.conf.js
@@ -4,51 +4,20 @@
 
 "use strict";
 const { karmaConfig } = require("@jsdevtools/karma-config");
-
 const { host } = require("./");
 
 module.exports = karmaConfig({
   sourceDir: "esm",
   browsers: {
-    ie: true,
-    edge: true,
-    chrome: true,
-    safari: true,
+    chrome: host.os.linux,
     firefox: host.os.linux,
+    safari: host.os.linux,    // SauceLabs
+    edge: host.os.linux,      // SauceLabs
+    ie: host.os.windows,
   },
   config: {
     // Prevent Karma-Config from using the "karma-host-environment" framework,
     // which would cause obvious conflicts with this project
     frameworks: ["mocha"],
-
-    reporters: [
-      "verbose",
-      "coverage-istanbul",
-      "saucelabs"
-    ],
-
-    logLevel: "debug",
-    captureTimeout: 60000,
-    browserDisconnectTolerance: 5,
-    browserDisconnectTimeout: 60000,
-    browserNoActivityTimeout: 60000,
-
-    saucelabs: {
-      build: "test " + Date.now(),
-      testName: "test " + Date.now(),
-    },
-
-    browsers: [
-      "Safari_SauceLabs"
-    ],
-
-    customLaunchers: {
-      // eslint-disable-next-line camelcase
-      Safari_SauceLabs: {
-        base: "SauceLabs",
-        platform: "macOS 10.15",
-        browserName: "safari",
-      }
-    }
   }
 });
